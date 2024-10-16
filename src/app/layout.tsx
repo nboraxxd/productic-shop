d9b7e62927553518d258f5b7c5c76126727c7a32
nboraxxd <nhatbaoxxd@gmail.com>
2024-10-16 21:35:44 +0700

    Using google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,135 +1,7 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 
 import { TanstackQueryProvider } from '@/components/provider'
 import '@/app/globals.css'
-
-const nunito = localFont({
-  src: [
-    {
-      path: './fonts/nunito/Nunito-VariableFont_wght.ttf',
-      style: 'normal',
-    },
-    {
-      path: './fonts/nunito/Nunito-Italic-VariableFont_wght.ttf',
-      style: 'italic',
-    },
-  ],
-  display: 'swap',
-  variable: '--font-nunito',
-})
-
-const arimo = localFont({
-  src: [
-    {
-      path: './fonts/arimo/Arimo-VariableFont_wght.ttf',
-      style: 'normal',
-    },
-    {
-      path: './fonts/arimo/Arimo-Italic-VariableFont_wght.ttf',
-      style: 'italic',
-    },
-  ],
-  display: 'swap',
-  variable: '--font-arimo',
-})
-
-const kanit = localFont({
-  src: [
-    {
-      path: './fonts/kanit/Kanit-Thin.ttf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-ThinItalic.ttf',
-      weight: '100',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-ExtraLight.ttf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-ExtraLightItalic.ttf',
-      weight: '200',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-Light.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-LightItalic.ttf',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-Italic.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-MediumItalic.ttf',
-      weight: '500',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-SemiBoldItalic.ttf',
-      weight: '600',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-BoldItalic.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-ExtraBold.ttf',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-ExtraBoldItalic.ttf',
-      weight: '800',
-      style: 'italic',
-    },
-    {
-      path: './fonts/kanit/Kanit-Black.ttf',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './fonts/kanit/Kanit-BlackItalic.ttf',
-      weight: '900',
-      style: 'italic',
-    },
-  ],
-  display: 'swap',
-  variable: '--font-kanit',
-})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -143,7 +15,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${kanit.className} ${nunito.variable} ${arimo.variable} ${kanit.variable} antialiased`}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body className={`antialiased`}>
         <TanstackQueryProvider>{children}</TanstackQueryProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">Home</h1>
-      <p className="font-arimo text-lg">Welcome to your new Next.js site!</p>
-      <p className="font-nunito text-lg">This is a starter template for your new project.</p>
+      <p className="poppins-extralight text-lg">Welcome to your new Next.js site!</p>
+      <p className="poppins-bold text-lg">This is a starter template for your new project.</p>
       <Button>Click me</Button>
     </main>
   )
